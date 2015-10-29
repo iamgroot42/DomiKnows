702f11786050da3e6f2c56aeed0918c43d34c8e4
Anshuman <anshuman14021@iiitd.ac.in>
2015-10-29 20:52:22 +0530

    Added servlets,blah blah blah...

--- src/TrackOrder.java
+++ src/TrackOrder.java
@@ -1,9 +1,6 @@
 
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,16 +9,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class Home
+ * Servlet implementation class TrackOrder
  */
-@WebServlet("/Home")
-public class Home extends HttpServlet {
+@WebServlet("/TrackOrder")
+public class TrackOrder extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Home() {
+    public TrackOrder() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -38,27 +35,7 @@ public class Home extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.setContentType("text/html");
-		PrintWriter writer = response.getWriter();
-		writer.print("<html>");
-		writer.print("<head>");
-		writer.print("<title>");
-		writer.print("Contact Info");
-		writer.print("</title>");
-		writer.print("</head>");
-		writer.print("<body>");
-		writer.print("<h2> Welcome to Pizza Planet! </h2>");
-		writer.print("</br></br>");
-		writer.print("<form action=\"PizzaOrder\" method=\"get\">");
-		writer.print("<input type=\"submit\" value=\"Place Order\"/>");
-		writer.print("</form>");
-		writer.print("</br></br>");
-		writer.print("<form action=\"TrackOrder\" method=\"get\">");
-		writer.print("<input type=\"submit\" value=\"Track Order\"/>");
-		writer.print("</form>");
-		writer.print("</body>");
-		writer.print("</html>");	
-		if(request.getParameter("place-order")!=null) System.out.println("TOMATO");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
